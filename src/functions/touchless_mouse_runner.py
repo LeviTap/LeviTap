b2aa32dad794c5a5f6444859f2742f6b4b9806af
Saksham Jain <sjain99@wisc.edu>
2023-11-14 18:47:17 -0600

    moved functions files to new repo

--- src/functions/touchless_mouse_runner.py
+++
@@ -1,7 +0,0 @@
-from multiprocessing import Process
-from touchless_mouse import touchless_mouse
-
-if __name__ == "__main__":
-    touchless_process = Process(target=touchless_mouse)
-    touchless_process.start()
-    touchless_process.join()
